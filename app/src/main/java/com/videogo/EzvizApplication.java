d82071fdb90c324e4d11b1cdd81f0c6d79c8db33
1003021685 <1003021685@qq.com>
2019-07-26 10:59:48 +0800

    10.59

--- app/src/main/java/com/videogo/EzvizApplication.java
+++ app/src/main/java/com/videogo/EzvizApplication.java
@@ -16,9 +16,18 @@
 package com.videogo;
 
 import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.videogo.constant.Constant;
 import com.videogo.openapi.EZOpenSDK;
+import com.videogo.openapi.bean.EZAccessToken;
+import com.videogo.ui.cameralist.EZCameraListActivity;
+import com.videogo.util.LogUtil;
 
 /**
  * 自定义应用
@@ -27,8 +36,10 @@ import com.videogo.openapi.EZOpenSDK;
 public class EzvizApplication extends Application {
 
     //开发者需要填入自己申请的appkey//ABC
-    public static String AppKey = "76d8a02ae81a4260a02e470ebb48077d";
-
+    //public static String AppKey = "76d8a02ae81a4260a02e470ebb48077d";
+    public static String AppKey = "bec27f333fd04a95a352bec49d466754";
+    private IntentFilter intentFilter;
+    private EzvizBroadcastReceiver receiver;
     public static EZOpenSDK getOpenSDK() {
         return EZOpenSDK.getInstance();
     }
@@ -37,7 +48,23 @@ public class EzvizApplication extends Application {
     public void onCreate() {
         super.onCreate();
         initSDK();
+        initData();
         SDKInitializer.initialize(this);
+    }
+
+    private void initData() {
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.videogo.action.ADD_DEVICE_SUCCESS_ACTION");
+        intentFilter.addAction("com.videogo.action.OAUTH_SUCCESS_ACTION");
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        receiver = new EzvizBroadcastReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        unregisterReceiver(receiver);
     }
 
     private void initSDK() {
@@ -56,6 +83,22 @@ public class EzvizApplication extends Application {
              * APP_KEY请替换成自己申请的
              */
             EZOpenSDK.initLib(this, AppKey);
+        }
+    }
+    private class EzvizBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            LogUtil.i("TAG","action = "+action);
+            if (action.equals(Constant.OAUTH_SUCCESS_ACTION)){
+                Log.i("TAG", "onReceive: OAUTH_SUCCESS_ACTION");
+                Intent i = new Intent(context, EZCameraListActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                /*******   获取登录成功之后的EZAccessToken对象   *****/
+                EZAccessToken token =EzvizApplication.getOpenSDK().getEZAccessToken();
+                context.startActivity(i);
+            }
         }
     }
 }
