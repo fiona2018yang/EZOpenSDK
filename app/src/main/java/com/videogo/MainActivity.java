2da9501e3c9b10789d1588cfd7a354f6955854f6
1003021685 <1003021685@qq.com>
2019-07-30 11:31:50 +0800

    7/30

--- app/src/main/java/com/videogo/MainActivity.java
+++ app/src/main/java/com/videogo/MainActivity.java
@@ -1,34 +1,52 @@
 package com.videogo;
 
 import android.Manifest;
-import android.app.Activity;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.mapping.view.MapView;
 
 import ezviz.ezopensdk.R;
 
 public class MainActivity extends FragmentActivity {
-    private ViewPager mViewPager;
-    private List<Fragment> list_frag =new ArrayList<>();
-    //private FragmentPagerAdapter mAdapter;
-    private FragmentStatePagerAdapter mAdapter;
-    private MapFrag mapFrag;
-    private CameraListFrag cameraListFrag;
+    private MapView map = null;
+    //屏幕点
+    private Point mapPoint;
+    //画点、线、面、涂鸦辅助图层
+    private GraphicsLayer drawLayer;
+    //画图工具
+    private DrawTool drawTool;
+    //地图储存位置
+    private String earthPath = "";
+    private String path;
+    private ArcGISLocalTiledLayer localtitleLayer;
+    private SharedPreferences sharedPreferences;
+
+    private ImageView amplify, narrow, distmeasure, areameasure, clean, back, openFile, xiazai;
+    private CheckBox local;
+    private int isChecked = 0; //0为清空，1为画线，2为画面
+    private LinearLayout linearLayout;
+    private TextView txy, tsc, tre, result;
+
+    private MyApplication app = (MyApplication) x.app();
+    //数据更新频率
+    private long gpstime;
+    private Point gpsPoint, myPoint;
+    private boolean isTrue = true;
+
     private static String[] allpermissions = {
             Manifest.permission.INTERNET,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -43,50 +61,12 @@ public class MainActivity extends FragmentActivity {
         checkpermission();
     }
     private void initViews() {
-        mViewPager = findViewById(R.id.viewpager);
-        mapFrag = new MapFrag();
-        list_frag.add(mapFrag);
-        cameraListFrag = new CameraListFrag();
-        list_frag.add(cameraListFrag);
-        mAdapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                return list_frag.get(position);
-            }
 
-            @Override
-            public int getCount() {
-                return list_frag.size();
-            }
-        };
-//        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-//            @Override
-//            public Fragment getItem(int position) {
-//                return list_frag.get(position);
-//            }
-//
-//            @Override
-//            public int getCount() {
-//                return list_frag.size();
-//            }
-//        };
-        mViewPager.setAdapter(mAdapter);
-    }
-
+        }
     @Override
     protected void onResume() {
         super.onResume();
         checkpermission();
-    }
-    public void reloadFragView(){
-        if (list_frag.contains(mapFrag)){
-            list_frag.remove(mapFrag);
-            getSupportFragmentManager().beginTransaction().remove(mapFrag).commit();
-            mapFrag = new MapFrag();
-            list_frag.add(mapFrag);
-            Log.i("TAG","reload");
-            mAdapter.notifyDataSetChanged();
-        }
     }
     /**
      * 权限管理
